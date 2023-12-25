ba2e8f33751b71f37aa607361d1e8cc3c092fccf
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-12-25 12:59:34 +0200

    Make API requests with Axios

---
+++ api/PokemonApi.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+interface Pokemon {
+	name: string;
+	sprites: {
+		front_default: string;
+	};
+}
+
+export const getAllPokemons = async (): Promise<Pokemon[]> => {
+	try {
+		const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151');
+
+		return data.results;
+	} catch (error) {
+		console.error('Error fetching Pokémon data:', error);
+		throw error;
+	}
+};
+
+export const getPokemonDetails = async (pokemonName: string): Promise<Pokemon> => {
+	try {
+		const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+		return data;
+	} catch (error) {
+		console.error('Error fetching Pokémon details:', error);
+		throw error;
+	}
+};
